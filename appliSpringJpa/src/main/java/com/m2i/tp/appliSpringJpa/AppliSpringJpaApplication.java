023ad4790bcfd0671d74d0107fcf70289128e88c
DidierDefrance <didier@d-defrance.fr>
2022-05-18 10:42:24 +0200

    debut projet

---
+++ appliSpringJpa/src/main/java/com/m2i/tp/appliSpringJpa/AppliSpringJpaApplication.java
@@ -0,0 +1,14 @@
+package com.m2i.tp.appliSpringJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppliSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppliSpringJpaApplication.class, args);
+		System.out.println("http://localhost:8080/appliSpringJpa");
+	}
+
+}
