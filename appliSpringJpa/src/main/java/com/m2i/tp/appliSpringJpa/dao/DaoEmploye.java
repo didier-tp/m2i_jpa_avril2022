cbc7a2df67d083740a629f1f6c2ca0f891490dfb
DidierDefrance <didier@d-defrance.fr>
2022-05-18 14:26:35 +0200

    interface dao

---
+++ appliSpringJpa/src/main/java/com/m2i/tp/appliSpringJpa/dao/DaoEmploye.java
@@ -0,0 +1,24 @@
+package com.m2i.tp.appliSpringJpa.dao;
+
+import java.util.List;
+
+import com.m2i.tp.appliSpringJpa.entity.Employe;
+
+/*
+ DAO : Data Access Object (objet de traitement spécialisé dans l'accès aux données en base)
+ comporte des méthodes CRUD (Create/Insert , Rechercher , Update , Delete)
+ avec throws RuntimeException implicite
+ */
+
+public interface DaoEmploye {
+      public Employe findById(Integer id);
+      public List<Employe> findAll();
+      //....
+      public Employe insertEmploye(Employe emp);//en entrée : objet pas encore stocké en base avec idEmp=null
+                                                //en retour : objet avec .idEmp auto-incrémenté
+      
+      public void updateEmploye(Employe emp); // update / mise à jour des valeurs en base
+      
+      public void deleteById(Integer id);
+      
+}
