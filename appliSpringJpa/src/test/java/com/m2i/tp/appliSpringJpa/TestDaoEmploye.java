07916d1d0a1328d5ba56d9276b2a3211b4946d75
DidierDefrance <didier@d-defrance.fr>
2022-05-18 17:01:04 +0200

    jpa avec spring

---
+++ appliSpringJpa/src/test/java/com/m2i/tp/appliSpringJpa/TestDaoEmploye.java
@@ -0,0 +1,26 @@
+package com.m2i.tp.appliSpringJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.m2i.tp.appliSpringJpa.dao.DaoEmploye;
+import com.m2i.tp.appliSpringJpa.entity.Employe;
+
+@SpringBootTest
+class TestDaoEmploye {
+	
+	//initialise daoEmploye pour que ça référence un composant pris en charge
+	//par Spring et qui est compatible avec l'interface DaoEmploye
+	//dans ce projet , seule la classe DaoEmployeJpa correspond à ce critère
+	@Autowired //injection de dépendance via Spring
+	private DaoEmploye daoEmploye;
+
+	@Test
+	void testAjoutEtRecuperation() {
+		Employe emp1 = new Employe(null, "prenom1", "Nom", "0102030405", "jean.Bon@xyz.com", "login", "pwd");
+		Employe empl1StockeEnBase = daoEmploye.insertEmploye(emp1);
+		System.out.println("empl1StockeEnBase"+empl1StockeEnBase.toString());
+	}
+
+}
