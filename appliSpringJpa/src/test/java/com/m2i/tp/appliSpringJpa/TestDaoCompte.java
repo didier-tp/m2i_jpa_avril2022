2bb05a83dc7ba5e570a0b77d99d55a3e21980f81
DidierDefrance <didier@d-defrance.fr>
2022-05-19 09:54:07 +0200

    avant astuce dao

---
+++ appliSpringJpa/src/test/java/com/m2i/tp/appliSpringJpa/TestDaoCompte.java
@@ -0,0 +1,24 @@
+package com.m2i.tp.appliSpringJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.m2i.tp.appliSpringJpa.dao.DaoCompte;
+import com.m2i.tp.appliSpringJpa.entity.Compte;
+
+@SpringBootTest
+class TestDaoCompte {
+	
+
+	@Autowired //injection de dépendance via Spring
+	private DaoCompte daoCompte;
+
+	@Test
+	void testAjoutEtRecuperation() {
+		Compte compteA = new Compte(null, "compteA", 120.0);
+		Compte compteAStockeEnBase = daoCompte.insertCompte(compteA);
+		System.out.println("compteAStockeEnBase"+compteAStockeEnBase.toString());
+	}
+
+}
