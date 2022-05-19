a993f635e5e4ca3604c9ee1a116b5b70c18ba7b9
DidierDefrance <didier@d-defrance.fr>
2022-05-19 11:26:14 +0200

    test crud

--- appliSpringJpa/src/main/java/com/m2i/tp/appliSpringJpa/dao/DaoEmployeJpaAvecSpring.java
+++ appliSpringJpa/src/main/java/com/m2i/tp/appliSpringJpa/dao/DaoEmployeJpaAvecSpring.java
@@ -25,45 +25,12 @@ import com.m2i.tp.appliSpringJpa.entity.Employe;
 */
 @Repository //cas particulier de @Component //pour prise en charge par framework spring
 @Transactional //pour commit/rollback automatique
-public class DaoEmployeJpaAvecSpring implements DaoEmploye {
+public class DaoEmployeJpaAvecSpring extends DaoGenericJpa<Employe,Integer> implements DaoEmploye {
+
+	public DaoEmployeJpaAvecSpring() {
+		super();
+		this.entityClass = Employe.class;
+	}
 	
-	//NB: @PersistenceContext permet d'initialiser l'objet technique
-	//entityManager à partir d'une configuration
-	// src/main/resources/META-INF/persistence.xml
-	// ou bien config spring équivalente dans src/main/resources/application.properties
-	@PersistenceContext
-	private EntityManager entityManager;
 	
-
-	@Override
-	public Employe findById(Integer id) {
-		return entityManager.find(Employe.class, id);//.find() au sens .findByPrimaryKey()
-	}
-
-	@Override
-	public List<Employe> findAll() {
-		return entityManager.createQuery("SELECT e FROM Employe e",Employe.class)
-				.getResultList();
-	}
-
-	@Override
-	public Employe insert(Employe emp) {
-			//en entrée , emp est un nouvel objet employé avec .empId à null (encore inconnu)
-			//déclenche automatiquement INSERT INTO Employe(firstname, ....) VALUES(emp.getFirstname() , ....)
-			entityManager.persist(emp); //INSERT INTO SQL avec aut_incr
-		return emp;//avec .empId auto incrémenté
-	}
-
-	@Override
-	public void update(Employe emp) {
-			entityManager.merge(emp); //UPDATE SQL
-	}
-
-	@Override
-	public void deleteById(Integer id) {
-			Employe empAsupprimer = entityManager.find(Employe.class, id);
-			entityManager.remove(empAsupprimer);
-			//déclenche automatiquement DELETE FROM Employe WHERE idEmp=code
-	}
-
 }
--- appliSpringJpa/src/test/java/com/m2i/tp/appliSpringJpa/TestDaoCompte.java
+++ appliSpringJpa/src/test/java/com/m2i/tp/appliSpringJpa/TestDaoCompte.java
@@ -1,5 +1,8 @@
 package com.m2i.tp.appliSpringJpa;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,10 +18,33 @@ class TestDaoCompte {
 	private DaoCompte daoCompte;
 
 	@Test
-	void testAjoutEtRecuperation() {
+	void testCRUD() {
 		Compte compteA = new Compte(null, "compteA", 120.0);
 		Compte compteAStockeEnBase = daoCompte.insert(compteA);
 		System.out.println("compteAStockeEnBase"+compteAStockeEnBase.toString());
+		Integer idCpt = compteAStockeEnBase.getNumero();
+		
+		List<Compte> listComptes =  daoCompte.findAll();
+		System.out.println("listComptes="+listComptes);
+		Assertions.assertTrue(listComptes.size()>=1);
+		
+		Compte cptRelu = daoCompte.findById(idCpt);
+		System.out.println("cptRelu="+cptRelu);
+		Assertions.assertEquals(compteA.getLabel(),cptRelu.getLabel());
+		
+		cptRelu.setLabel("nouveau label"); cptRelu.setSolde(140.0);
+		daoCompte.update(cptRelu);
+		
+		Compte cptReluApresModif = daoCompte.findById(idCpt);
+		System.out.println("cptReluApresModif="+cptReluApresModif);
+		Assertions.assertEquals(cptReluApresModif.getSolde(),140.0,0.0001);
+		
+		
+		daoCompte.deleteById(idCpt);
+		Compte cptReluApresSuppression = daoCompte.findById(idCpt);
+		System.out.println("cptReluApresSuppression="+cptReluApresSuppression);
+		Assertions.assertNull(cptReluApresSuppression);
+		
 	}
 
 }
